a6946194f620020ea00cee47a506a2d2fdb2f46a
Fawaz Sammani <sammanifawaz@gmail.com>
2019-11-22 00:21:57 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -30,8 +30,7 @@ def create_masks(question, reply_input, reply_target):
         mask = torch.triu(torch.ones(size, size)).transpose(0, 1).type(dtype=torch.uint8)
         return mask.unsqueeze(0)
     
-    question_mask = question!=0
-    question_mask = question_mask.to(device)
+    question_mask = (question!=0).to(device)
     question_mask = question_mask.unsqueeze(1).unsqueeze(1)         # (batch_size, 1, 1, max_words)
      
     reply_input_mask = reply_input!=0
